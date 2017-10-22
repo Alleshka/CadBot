d226285216b3ffeb111e8dbc0d51129d793b500a
grebovod_aleksei@mail.ru <grebovod_aleksei@mail.ru>
2017-10-22 20:52:02 +0300

    Снова начинаем работу над ботом

---
+++ CadBot.Code/CADbot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CadBot.Code
+{
+    /// <summary>
+    /// Класс работы бота
+    /// </summary>
+    public class CADbot
+    {
+        public bool _mainThread;
+        public bool _updateThread;
+        public bool _actionThread;
+    }
+}
